2e79def41dfe93d0be52564be1d415b18cab4337
David <1697252093@qq.com>
2023-12-01 19:41:01 +0800

    fix the bug of SuccessfulDefense of Player

--- src/main/java/uoa/assignment/character/Player.java
+++ src/main/java/uoa/assignment/character/Player.java
@@ -2,32 +2,26 @@ package uoa.assignment.character;
 
 import java.util.Random;
 
-public class Player extends GameCharacter{
+public class Player extends GameCharacter {
 
 	public Player(String name) {
 		super(name);
 	}
 
-	
-	public
-	void hurtCharacter(GameCharacter character) {
-		if (character.successfulDefense()){
+	public void hurtCharacter(GameCharacter character) {
+		if (character.successfulDefense()) {
 			return;
-		}
-		else {
+		} else {
 			character.setHealth(character.getHealth() - 50);
 		}
 	}
 
-	
-	public
-	boolean successfulDefense() {
+	public boolean successfulDefense() {
 		Random rand = new Random();
-		int randomNum = rand.nextInt(4);
-		if (randomNum == 0) {
+		int randomNum = rand.nextInt(10);
+		if (randomNum < 3) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
 	}
